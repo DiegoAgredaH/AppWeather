8a64b28a01ee3391eb39c5b92134eaab30a1dc04
DiegoAgreda <50123251+DiegoAgreda@users.noreply.github.com>
2021-05-03 23:20:34 -0500

    Creacion del componente ForecastChart el cual muestra el grafico en un plano cartesiano

---
+++ src/components/ForecastChart/ForecastChart.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+//LineChart nos sirve para poder crear la grafica
+//CartesianGrid para agregar un plano cartesiano
+//Xaxis para agregar el eje "X" y YAxis para el eje "Y"
+//Tooltip para que cuando pasemos el mouse por cada uno de los puntos de la grafica nos muestre informacion más detallada
+//Legen nos indica con que color se representa cada linea
+import { LineChart, 
+        Line, 
+        CartesianGrid, 
+        XAxis, 
+        YAxis,
+        Tooltip, 
+        Legend } from 'recharts'
+
+const ForecastChart = ({ data }) => {
+    return (
+        <div>
+            <LineChart
+                height={250}
+                width={700}
+                margin={{top: 20, bottom: 20, left: 5, right: 5}}
+                data={data} >
+                    <XAxis dataKey="dayHour"></XAxis>
+                    <YAxis></YAxis>
+                    <CartesianGrid></CartesianGrid>
+                    <Tooltip></Tooltip>
+                    <Legend></Legend>
+                    <Line type="monotone" dataKey="max" stroke="#FF0000"></Line>
+                    <Line type="monotone" dataKey="min" stroke="#0000FF"></Line>
+            </LineChart>
+        </div>
+    )
+}
+
+ForecastChart.propTypes = {
+    data: PropTypes.arrayOf( 
+        PropTypes.shape({
+        dayHour: PropTypes.string.isRequired,
+        min: PropTypes.number.isRequired,
+        max: PropTypes.number.isRequired,
+    }),
+    ).isRequired,
+}
+
+export default ForecastChart
